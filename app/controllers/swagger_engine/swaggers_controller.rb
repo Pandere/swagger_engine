abaa70f02b4b2e7a40d42672bb40686a31dd81e0
MattHall <matthew.hall@gmail.com>
2016-06-30 12:22:53 +0100

    Refactor json_files

--- app/controllers/swagger_engine/swaggers_controller.rb
+++ app/controllers/swagger_engine/swaggers_controller.rb
@@ -4,17 +4,15 @@ module SwaggerEngine
   class SwaggersController < ApplicationController
     layout false
 
-    before_filter :load_json_files
-
     def index
-      redirect_to swagger_path(@json_files.first[0]) if ( @json_files.size == 1 )
+      redirect_to swagger_path(json_files.first[0]) if ( json_files.size == 1 )
     end
 
     def show
       respond_to do |format|
         format.html {
           if json_file[0] == '/'
-            @swagger_json_url = json_file[0]
+            @swagger_json_url = json_file
           else
             @swagger_json_url = swagger_path(params[:id], format: :json)
           end
@@ -28,10 +26,10 @@ module SwaggerEngine
     private
 
     def json_file
-      @json_file ||= @json_files[params[:id].to_sym]
+      @json_file ||= json_files[params[:id].to_sym]
     end
 
-    def load_json_files
+    def json_files
       @json_files ||= SwaggerEngine.configuration.json_files || { default: "#{Rails.root}/lib/swagger_engine/swagger.json" }
     end
   end
